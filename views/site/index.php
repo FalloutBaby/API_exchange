83386bc926ab2154a2a2b74047c679de596bb251
FalloutBaby <ferrashe4ka@mail.ru>
2019-09-21 17:42:11 +0300

    Project init

---
+++ views/site/index.php
@@ -0,0 +1,11 @@
+<?php
+/* @var $this yii\web\View */
+
+$this->title = 'Тестовое задание';
+?>
+<div class="site-index">
+
+    <?= \yii\helpers\Html::a('Работа с биржами', ['market/market'], ['class' => 'btn btn-lg btn-success']); ?>
+   
+
+</div>
